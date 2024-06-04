771c2a069fa03fc09ddccbc0a6e212840908acc9
Aluno <Aluno@10.85.198.42>
2024-06-04 16:55:23 -0300

    java project Chickens02

---
+++ src/Chickens02.java
@@ -0,0 +1,14 @@
+ public class Chickens02 {
+    public static void main(String[] args) {
+        //Put yout code here
+        double mediadiaria, mediamensal, lucromensal;
+        mediadiaria = (double) 122.66666666667;
+        mediamensal = (double) 3380.0;
+        lucromensal = (double) 608.8;
+        
+        System.out.println("Media diaria: " +mediadiaria);
+        System.out.println("Media mensal: " +mediamensal);
+        System.out.println("Lucro mensal: $" +lucromensal);
+    }
+    
+}
